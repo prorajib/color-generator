676fa1e7766af83bda26d7d6579e833ef04f0220
Rajib Chandra Das <prorajib6@gmail.com>
2022-11-06 21:52:53 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,53 @@ import SingleColor from './SingleColor';
 import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>;
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#776fdb').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(100);
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <section className='container'>
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#776fdb'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            generate
+          </button>
+        </form>
+      </section>
+      ;
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App;
